7d09f6b7ce3d2a489bd4b20fb1034304df92b316
Vova Zelenyi <vova2010201040@gmail.com>
2023-08-15 17:27:04 +0200

    Adapted Initialize method for MaintenanceConfig

--- src/Kirel.Identity.Server.Infrastructure/Contexts/IdentityServerDbInitializer.cs
+++ src/Kirel.Identity.Server.Infrastructure/Contexts/IdentityServerDbInitializer.cs
@@ -3,131 +3,89 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Kirel.Identity.Server.Infrastructure.Contexts;
-
-/// <summary>
-/// Class responsible for initializing the Identity Server database
-/// </summary>
-public static class IdentityServerDbInitializer
+namespace Kirel.Identity.Server.Infrastructure.Contexts
 {
     /// <summary>
-    /// Initializes the Identity Server database with roles and users.
+    /// Class responsible for initializing the Identity Server database
     /// </summary>
-    /// <typeparam name="TDbContext"> The type of the database context. </typeparam>
-    /// <param name="serviceProvider"> The service provider. </param>
-    /// <param name="maintenanceConfig"> The maintenance configuration. </param>
-    /// <param name="dataSeedConfig"> The dataseed configuration </param>
-    public static async Task Initialize<TDbContext>(IServiceProvider serviceProvider,
-        MaintenanceConfig maintenanceConfig, DataSeedConfig dataSeedConfig)
-        where TDbContext : DbContext
+    public static class IdentityServerDbInitializer
     {
-         var dataContext = serviceProvider.GetRequiredService<TDbContext>();
-        await dataContext.Database.MigrateAsync();
-
-        var roleManager = serviceProvider.GetRequiredService<RoleManager<Role>>();
-        var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
-        var adminRole = await roleManager.FindByNameAsync("Admin");
-        var markerFilePath = "dataseed.lock";
-
-        if (adminRole == null)
+        /// <summary>
+        /// Initializes the Identity Server database with roles and users.
+        /// </summary>
+        /// <typeparam name="TDbContext">The type of the database context.</typeparam>
+        /// <param name="serviceProvider">The service provider.</param>
+        /// <param name="maintenanceConfig">The maintenance configuration.</param>
+        public static async Task Initialize<TDbContext>(IServiceProvider serviceProvider, MaintenanceConfig maintenanceConfig)
+            where TDbContext : DbContext
         {
-            if (adminRole == null)
-            {
-                adminRole = new Role { Name = "Admin", NormalizedName = "ADMIN" };
-                var result = await roleManager.CreateAsync(adminRole);
-                if (!result.Succeeded)
-                {
-                }
-            }
+            var dataContext = serviceProvider.GetRequiredService<TDbContext>();
+            await dataContext.Database.MigrateAsync();
+            
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<Role>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
 
-            var adminUserExist = true;
-            var adminUser = await userManager.FindByNameAsync("Admin");
-            if (adminUser == null)
+            if (maintenanceConfig?.Admin?.Reset == true)
             {
-                adminUser = new User()
-                {
-                    UserName = "Admin", Email = "admin@citmed.ru", Name = "Admin", LastName = "Default"
-                };
-                var result = await userManager.CreateAsync(adminUser, "Admin@123");
-                if (!result.Succeeded)
-                    adminUserExist = false;
-            }
-
-            if (adminUserExist && maintenanceConfig.Admin.Reset)
-            {
+                var adminUser = await userManager.FindByNameAsync("Admin");
                 var newPassword = maintenanceConfig.Admin.Password ?? "Admin@123";
                 await userManager.RemovePasswordAsync(adminUser);
                 await userManager.AddPasswordAsync(adminUser, newPassword);
             }
 
-          
-        }
-
-        foreach (var roleConfig in dataSeedConfig.Roles)
-        {
-            var roleName = roleConfig.Name;
-            var roleNormalizedName = roleConfig.NormalizedName;
-
-            var roleExist = await roleManager.RoleExistsAsync(roleName);
-            if (!roleExist)
+            foreach (var roleConfig in maintenanceConfig!.Roles)
             {
-                var role = new Role { Name = roleName, NormalizedName = roleNormalizedName };
-                var result = await roleManager.CreateAsync(role);
-                if (!result.Succeeded)
-                {
-                    foreach (var error in result.Errors)
-                    {
-                        var errorMessage = $"Error creating role: {error.Description}";
-                        throw new Exception(errorMessage);
-                    }
+                var roleName = roleConfig.Name;
+                var roleNormalizedName = roleConfig.NormalizedName;
 
-                    throw new Exception($"Role name: {roleName}");
+                var roleExist = await roleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    var role = new Role { Name = roleName, NormalizedName = roleNormalizedName };
+                    var result = await roleManager.CreateAsync(role);
+                    if (!result.Succeeded)
+                    {
+                        
+                    }
                 }
             }
-        }
 
-        foreach (var userConfig in dataSeedConfig.Users)
-        {
-            var userName = userConfig.UserName;
-            var email = userConfig.Email;
-            var name = userConfig.Name;
-            var lastName = userConfig.LastName;
-            var password = userConfig.Password;
-
-            var userExist = await userManager.FindByNameAsync(userName);
-            if (userExist == null)
+            foreach (var userConfig in maintenanceConfig.Users)
             {
-                var user = new User
+                var userName = userConfig.UserName;
+                var email = userConfig.Email;
+                var name = userConfig.Name;
+                var lastName = userConfig.LastName;
+                var password = userConfig.Password;
+
+                var userExist = await userManager.FindByNameAsync(userName);
+                if (userExist == null)
                 {
-                    UserName = userName,
-                    Email = email,
-                    Name = name,
-                    LastName = lastName
-                };
-                var result = await userManager.CreateAsync(user, password);
-                if (!result.Succeeded)
-                {
-                    foreach (var error in result.Errors)
+                    var user = new User
                     {
-                        var errorMessage = $"Error creating user: {error.Description}";
-                        throw new Exception(errorMessage);
+                        UserName = userName,
+                        Email = email,
+                        Name = name,
+                        LastName = lastName
+                    };
+                    var result = await userManager.CreateAsync(user, password);
+                    if (!result.Succeeded)
+                    {
+                        
                     }
 
-                    throw new Exception($"User name: {userName}");
-                }
-
-                if (userConfig.Roles != null)
-                {
-                    foreach (var roleName in userConfig.Roles)
-                    {
-                        if (await roleManager.RoleExistsAsync(roleName))
+                    if (userConfig.Roles != null)
+                        foreach (var roleName in userConfig.Roles)
                         {
-                            await userManager.AddToRoleAsync(user, roleName);
+                            if (await roleManager.RoleExistsAsync(roleName))
+                            {
+                                await userManager.AddToRoleAsync(user, roleName);
+                            }
                         }
-                    }
                 }
             }
+
+            
         }
-        File.Create(markerFilePath).Dispose();
     }
 }
